fba7d402032f83c67eb3f5c210aeaaa0aa777f39
ShadowfeindX <shadowfeind@programmer.net>
2016-10-26 22:22:44 -0400

    Initial Commit

---
+++ todo_list.elm
@@ -0,0 +1,16 @@
+-- BEGIN
+module TodoList exposing (..)
+
+-- IMPORTS
+import Html exposing (..)
+
+-- MAIN
+main = div [] [
+  text "Todo List",
+  br [][],
+  text "- First Item",
+  br [][],
+  text "- Second Item",
+  br [][],
+  
+  ]
